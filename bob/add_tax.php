0c123fa4b1c0cd3014ba4ad4a8970e0e34de8581
zmijevik <persie.joseph@gmail.com>
2013-02-06 03:41:19 -0600

    bob bird tags

--- bob/add_tax.php
+++ bob/add_tax.php
@@ -34,57 +34,50 @@ taxonomy_save_term($term);
 //taxonomy_image_add_from_url(115,'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRSiZLaRxq7JsSrHlEww-1i5gR0f1s4yWY2IuF-zfKoKl1_QH4dk85k-w','fartsmoker.jpg');
 
 //delete taxonomy image
+/*
 
 $db = array(
 85,
-19,
-74,
-59,
-27,
-4,
-15,
-35,
-40,
-32,
-53,
-22,
-45,
-31,
-8,
-60,
-66,
-33,
-83,
-28,
-11,
-88,
-21,
-69,
-42,
-56,
-99,
-26,
-84,
-1,
-50,
-44,
-39,
-57,
-106,
-68,
-61,
-92,
-65,
-90,
-76,
-95,
-51,
 78
 );
 
 foreach($db as $d) 
     taxonomy_del_term($d);    
-
+*/
 
 
 //taxonomy_image_delete($d);
+
+// load a node
+
+/*
+
+$nid = 226;
+
+$node = node_load(array("nid" => $nid));
+
+var_dump($node);
+
+*/
+
+//create a node associated to an existing term
+
+/*
+
+$node = new stdClass();
+
+$node->name = 'Traits';
+$node->title = $node_name;
+$node->body = "<h3>Fart</h3>";
+$node->type = "trait";
+$node->status = 1;
+$node->created = time();
+$node->taxonomy = array(taxonomy_get_term(7));
+
+if ($node = node_submit($node)) {
+  node_save($node);
+}
+
+var_dump($node);
+
+*/
