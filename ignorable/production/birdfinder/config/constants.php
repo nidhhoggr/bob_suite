3cb7fffc3194bbd1a99fb70fd4b3f3e0f65fe188
zmijevik <persie.joseph@gmail.com>
2013-07-11 15:12:34 -0700

    no need for environment selection logic in constants file

--- ignorable/production/birdfinder/config/constants.php
+++ ignorable/production/birdfinder/config/constants.php
@@ -9,10 +9,6 @@ define("BF_VIEW_DIR", dirname(__FILE__). '/../views/');
 define("BF_UTIL_DIR", dirname(__FILE__). '/../utils/');
 
 $prodbox = true;
-//only for shell usage
-$sandbox = false;
-
-if(@strstr($_SERVER['HTTP_HOST'],'beautyofbirds.com') || $prodbox) {
 
     //SET THE CONNECTION GLOBALS HERE
     define('DBUSER','beautyof');
@@ -26,31 +22,3 @@ if(@strstr($_SERVER['HTTP_HOST'],'beautyofbirds.com') || $prodbox) {
     define('bird_managertemplate_url','http://beautyofbirds.com/birdfindermanager/web/frontend.php');
     define('bird_interface_url','http://beautyofbirds.com/birdfinder/interface/');
     define('bird_drupal_url','http://beautyofbirds.com/');
-}
-else if(@strstr($_SERVER['HTTP_HOST'],'supraliminalsolutions.com')) {
-
-    //SET THE CONNECTION GLOBALS HERE
-    define('DBUSER','zmijevik');
-    define('DBPASSWORD','a1genda666');
-    define('DBNAME','zmijevik_birdfinder');
-    define('DBHOST','localhost');
-    define('DBDRIVER','mysql');
-    define("PEAR_LIB",'/home/zmijevik/php/');
-
-    define('bird_manager_url','http://supraliminalsolutions.com/clients/sibylle/birdfindermanager/web/backend.php/');
-    define('bird_managertemplate_url','http://supraliminalsolutions.com/clients/sibylle/birdfindermanager/web/frontend.php');
-    define('bird_interface_url','http://supraliminalsolutions.com/clients/sibylle/birdfinder/interface/');
-} 
-else if(@strstr($_SERVER['HTTP_HOST'],'clients') || $sandbox) {
-
-    //SET THE CONNECTION GLOBALS HERE
-    define('DBUSER','root');
-    define('DBPASSWORD','root');
-    define('DBNAME','bob_birdfinder');
-    define('DBHOST','localhost');
-    define('DBDRIVER','mysql');
-
-    define('bird_manager_url','http://sfprojects/birdfindermanager/web/backend.php/');
-    define('bird_managertemplate_url','http://sfprojects/birdfindermanager/web/frontend.php');
-    define('bird_interface_url','http://clients/sibylle/birdsofbeauty/birdfinder/injection_engine/interface/');
-}
